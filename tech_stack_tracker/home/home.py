b577a6016e8dff7995f188dcbba1c42d895a6f02
bosslibra <alessandr.libralesso@studio.unibo.it>
2026-04-11 18:56:13 +0200

    fix missing template folder setup

--- tech_stack_tracker/home/home.py
+++ tech_stack_tracker/home/home.py
@@ -1,6 +1,11 @@
 from flask import Blueprint, render_template
 
-home_bp = Blueprint("home", __name__)
+home_bp = Blueprint(
+    "home",
+    __name__,
+    template_folder="templates",
+    static_folder="static"
+)
 
 
 # Home page route
